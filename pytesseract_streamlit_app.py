90692bb695b553cbfad753a234c561f0a78a10d2
 “Aihiang_Ng” <aihiang_ng@singaporeair.com.sg>
2022-02-16 14:14:10 +0800

    add requirements.txt

--- pytesseract_streamlit_app.py
+++ pytesseract_streamlit_app.py
@@ -1,76 +1,10 @@
 import pandas as pd
 import streamlit as st
-# from pytesserract_extractor import ocr_model
+from pytesserract_extractor import ocr_model
 # from ocr_info_extractor import ocr_model
 
 
 # Streamlit App
-from PIL import Image
-import pandas as pd
-import pytesseract
-
-
-# Classes
-class OCR:
-    def __init__(self):
-        pass
-    # def __init__(self, detection_model_name='DB_r18', recognizer_model_name='RobustScanner', device='cpu', merge=True,
-    #              details=True):
-        # self.ocr_model = MMOCR(config_dir="~/mmocr/configs/",
-        #                        det=detection_model_name,
-        #                        recog=recognizer_model_name,
-        #                        # kie='SDMGR',
-        #                        device=device)
-        # self.merge = merge
-        # self.details = details
-    
-   
-    def _read_jpg_oriented(self, filename):
-        im=Image.open(filename)
-        try:
-            image_exif = im._getexif()
-            image_orientation = image_exif[274]
-            if image_orientation in (2,'2'):
-                return im.transpose(Image.FLIP_LEFT_RIGHT)
-            elif image_orientation in (3,'3'):
-                return im.transpose(Image.ROTATE_180)
-            elif image_orientation in (4,'4'):
-                return im.transpose(Image.FLIP_TOP_BOTTOM)
-            elif image_orientation in (5,'5'):
-                return im.transpose(Image.ROTATE_90).transpose(Image.FLIP_TOP_BOTTOM)
-            elif image_orientation in (6,'6'):
-                return im.transpose(Image.ROTATE_270)
-            elif image_orientation in (7,'7'):
-                return im.transpose(Image.ROTATE_270).transpose(Image.FLIP_TOP_BOTTOM)
-            elif image_orientation in (8,'8'):
-                return im.transpose(Image.ROTATE_90)
-            else:
-                return im
-        except (KeyError, AttributeError, TypeError, IndexError):
-            return im
-    
-    def _get_text_df(self, input_img):
-        custom_config = r'--psm 11' #try 8 
-        # text = pytesseract.image_to_string(img, lang='eng', config=custom_config)
-        img_word_df = pytesseract.image_to_data(input_img, lang='eng', config=custom_config, output_type='data.frame')
-        img_word_df = img_word_df[img_word_df.conf > -1] # -1 returns Nan
-        img_word_df = img_word_df[img_word_df.conf >= 80] # -1 returns Nan
-    
-        return img_word_df
-
-    def get_text(self, image_path):
-        img = self._read_jpg_oriented(image_path)
-
-        # Pytesseract get text
-        img_word_df = self._get_text_df(img)
-        doc_str = " ".join(img_word_df['text'])
-       
-        return [img, doc_str]
-
-
-ocr_model = OCR()
-
-
 """
 # OCR Info Extractor
 """
